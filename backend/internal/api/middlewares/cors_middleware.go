594f8b969b68fc940a59c92f2ba1acacef6e73b4
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-09-12 22:27:30 +0200

    #45 adjusted allowed origins

--- backend/internal/api/middlewares/cors_middleware.go
+++ backend/internal/api/middlewares/cors_middleware.go
@@ -8,7 +8,7 @@ import (
 // CORSMiddleware applies the CORS settings to the router
 func CORSMiddleware(next http.Handler) http.Handler {
     corsHandler := cors.New(cors.Options{
-        AllowedOrigins:   []string{"*"}, // CHANGE THIS - ALLOW ONLY FRONTEND URL
+        AllowedOrigins:   []string{"http://localhost:*"}, // CHANGE THIS - ALLOW ONLY FRONTEND URL
         AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
         AllowedHeaders:   []string{"Authorization", "Content-Type"},
         AllowCredentials: true,
