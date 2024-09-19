40e5d56a9988d43cb21bc12aa826685a5e9d7254
Malte Bjoerklund <maltebjoerklund@gmail.com>
2024-09-19 15:27:18 +0200

    removing weatherHandler

--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -13,9 +13,9 @@ func NewRouter() http.Handler {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/search", handlers.Search).Methods("POST")
-	router.HandleFunc("/api/weather", handlers.Weather).Methods("GET")
+	router.HandleFunc("/api/weather", nil).Methods("GET")
 	router.HandleFunc("/api/register", nil).Methods("POST") // Add the register handler here
-	router.HandleFunc("/api/login", handlers.Login).Methods("POST")
+	router.HandleFunc("/api/login", nil).Methods("POST")
 	router.HandleFunc("/api/logout", nil).Methods("GET") // Add the logout handler here
 
 	// if environment is not production, allow all origins (*)
