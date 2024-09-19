ab337c68cd570da91634027ba8af3cc187c184bf
emilvn <emilvnielsen@hotmail.com>
2024-09-20 00:01:21 +0200

    #78 removed Firefox from list of browsers for Playwright to test, as it is bugged

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -8,15 +8,16 @@ const testUserEmail = process.env.TEST_LOGIN_EMAIL ?? "";
 const testUserPassword = process.env.TEST_LOGIN_PASSWORD ?? "";
 
 test("can login", async ({ page }) => {
-  await page.goto(`${baseUrl}/login`);
+  // set a timeout of 10 seconds(max time to wait for the page to load)
+  await page.goto(`${baseUrl}/login`, { timeout: 10000 });
 
   // fill in form
   await page.fill("#login-email", testUserEmail);
   await page.fill("#login-password", testUserPassword);
   await page.click("#login-button");
 
-  // wait 1 second
-  await page.waitForTimeout(1000);
+  // wait 5 second
+  await page.waitForTimeout(5000);
 
   // expect to be redirected to the search page.
   expect(page.url()).toBe(`${baseUrl}/`);
@@ -25,15 +26,16 @@ test("can login", async ({ page }) => {
 });
 
 test("can log in and then log out", async ({ page }) => {
-  await page.goto(`${baseUrl}/login`);
+  // set a timeout of 10 seconds(max time to wait for the page to load)
+  await page.goto(`${baseUrl}/login`, { timeout: 10000 });
 
   // fill in the form and submit it.
   await page.fill("#login-email", testUserEmail);
   await page.fill("#login-password", testUserPassword);
   await page.click("#login-button");
 
-  // Wait for 1 seconds
-  await page.waitForTimeout(1000);
+  // Wait for 5 seconds
+  await page.waitForTimeout(5000);
 
   // Expect to be redirected to the search page.
   expect(page.url()).toBe(`${baseUrl}/`);
@@ -41,8 +43,8 @@ test("can log in and then log out", async ({ page }) => {
   // Click the logout button
   await page.click("#login-logout-nav");
 
-  // Wait for 1 seconds
-  await page.waitForTimeout(1000);
+  // Wait for 5 seconds
+  await page.waitForTimeout(5000);
 
   // Expect to be redirected to the login page.
   expect(page.url()).toBe(`${baseUrl}/login`);
--- frontend/tests/weather.spec.ts
+++ frontend/tests/weather.spec.ts
@@ -6,7 +6,8 @@ const baseUrl = process.env.TEST_FRONTEND_URL;
 
 // generate playwright tests to test that weather page gets weather content from backend
 test("weather page gets weather content from backend", async ({ page }) => {
-  await page.goto(baseUrl + "/weather");
+  // set a timeout of 10 seconds(max time to wait for the page to load)
+  await page.goto(baseUrl + "/weather", { timeout: 10000 });
 
   // Check if the weather content is loaded
   const weatherContent = page.locator("#weather-content");
