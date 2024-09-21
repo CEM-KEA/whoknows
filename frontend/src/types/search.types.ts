951be853640e84cb40834d06ed5b8e50c59c079f
emilvn <emilvnielsen@hotmail.com>
2024-09-21 22:01:10 +0200

    #90 changed frontend to accommodate rework of search route on API

--- frontend/src/types/search.types.ts
+++ frontend/src/types/search.types.ts
@@ -1,11 +1,8 @@
-export interface ISearchRequest {
-  q: string;
-  language: string | null; // language code e.g. en
-}
-
 export interface ISearchResponse {
   data: {
     title: string;
     url: string;
+    content: string;
+    id: number;
   }[];
 }
