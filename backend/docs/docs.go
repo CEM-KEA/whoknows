91d07cc10208734073e1ad20de7f770e8ab872cd
emilvn <emilvnielsen@hotmail.com>
2024-09-29 20:57:01 +0200

    #136 updated docs after removing hardcoded ip

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -303,7 +303,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "52.169.32.176:8080",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "WhoKnows API",
