e4853fff19f2ba5e48fdd6f67e282112a5f1d55a
emilvn <emilvnielsen@hotmail.com>
2024-11-15 12:44:56 +0100

    #183 added redirect for / and /api to documentation, and added robots.txt and sitemap.xml

--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -14,6 +14,22 @@ import (
 func NewRouter() http.Handler {
 	router := mux.NewRouter()
 
+	 // robots.txt
+	router.HandleFunc("/api/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/robots.txt")
+	})
+
+	// sitemap.xml
+	router.HandleFunc("/api/sitemap.xml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/sitemap.xml")
+	})
+
+	// Redirect to Swagger API documentation
+	router.Handle("/", http.RedirectHandler("/api/swagger/", http.StatusMovedPermanently))
+	router.Handle("/api", http.RedirectHandler("/api/swagger/", http.StatusMovedPermanently))
+	router.Handle("/robots.txt", http.RedirectHandler("/api/robots.txt", http.StatusMovedPermanently))
+	router.Handle("/sitemap.xml", http.RedirectHandler("/api/sitemap.xml", http.StatusMovedPermanently))
+
 	// Swagger API documentation route - http://localhost:8080/swagger/index.html
 	router.PathPrefix("/api/swagger/").Handler(httpSwagger.WrapHandler)
 
