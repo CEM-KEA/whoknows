e2e50d387160c63df4c583125971f57f7cd59113
Emil V. Nielsen <emilvnielsen@hotmail.com>
2024-11-15 14:51:27 +0100

    #183 Fixes Zap scan warnings on backend (#197)
    * #183 added redirect for / and /api to documentation, and added robots.txt and sitemap.xml
    
    * #183 fix relative path
    
    * #183 changed allowed origins for test environment to be localhost
    
    * #183 added cache control header to prevent web cache deception
    
    * #183 added content type header to fileserve
    
    * #183 nocache middleware to try and mitigate web cache deception
    
    * #183 refactored no cache middleware to seperate file

---
+++ backend/internal/api/middlewares/no_cache_middleware.go
@@ -0,0 +1,12 @@
+package middlewares
+
+import "net/http"
+
+func NoCacheMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		next.ServeHTTP(w, r)
+	})
+}
