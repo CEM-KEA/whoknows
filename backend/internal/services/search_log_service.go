dd86720a8f1d85f59e3cc5b7bc8cc8724667125d
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-11-16 22:53:25 +0100

    #48 Enhance CreateSearchLog function with improved logging for search log creation process

--- backend/internal/services/search_log_service.go
+++ backend/internal/services/search_log_service.go
@@ -2,10 +2,28 @@ package services
 
 import (
 	"github.com/CEM-KEA/whoknows/backend/internal/models"
+	"github.com/CEM-KEA/whoknows/backend/internal/utils"
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 // CreateSearchLog creates a new search log in the database.
 func CreateSearchLog(db *gorm.DB, searchLog *models.SearchLog) error {
-	return db.Create(searchLog).Error
+	utils.LogInfo("Creating search log", logrus.Fields{
+		"query": searchLog.Query,
+	})
+
+	err := db.Create(searchLog).Error
+	if err != nil {
+		utils.LogError(err, "Failed to create search log", logrus.Fields{
+			"query": searchLog.Query,
+		})
+		return err
+	}
+
+	utils.LogInfo("Search log created successfully", logrus.Fields{
+		"query": searchLog.Query,
+		"id":    searchLog.ID,
+	})
+	return nil
 }
